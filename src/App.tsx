8d8f43a88cb618f394e103824283a934d03ba1ac
Piyush Prateek <piyushprateek231996@gmail.com>
2024-05-14 17:28:16 +0530

    scss added / component folder - added loader / adminsidebar , pages added files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import Loader from "./components/Loader";
 
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Products = lazy(() => import("./pages/Products"));
+const Transaction = lazy(() => import("./pages/Transaction"));
+const Customers = lazy(() => import("./pages/Customers"));
 
 const App = () => {
   return (
     <Router>
-      <Routes>
+      <Suspense fallback={<Loader />}>
 
-        <Route />
+        <Routes>
 
-      </Routes>
+          <Route
+            path="/"
+            element={
+              <Link to="/admin/dashboard">
+                <button>Visit Dashboard</button>
+              </Link>
+            }
+          />
+
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/product" element={<Products />} />
+          <Route path="/admin/customer" element={<Customers />} />
+          <Route path="/admin/transaction" element={<Transaction />} />
+
+        </Routes>
+      </Suspense>
+
     </Router>
   )
 }
